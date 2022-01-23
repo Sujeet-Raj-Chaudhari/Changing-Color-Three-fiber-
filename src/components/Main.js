d950afb37848b2f90c5b6e4621323d49c4a47d40
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-01-23 18:34:58 +0530

    Final Commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React, { useRef, Suspense, useState,  useEffect} from 'react'
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import {proxy, useSnapshot} from "valtio";
+import { HexColorPicker } from 'react-colorful';
 
 
 const state = proxy({
@@ -37,6 +38,11 @@ function Model({ ...props }) {
   
   const [hovered, set] = useState(null)
   
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
+  }, [hovered])
 
   return (
     <group ref={group} {...props} dispose={null}
@@ -120,7 +126,11 @@ function Model({ ...props }) {
 function Picker(){
     const snap = useSnapshot(state)
     return(
-        <div className = "picker">{snap.current}
+        <div style ={{display : snap.current ? "block": "none"}}>
+            <HexColorPicker className = "picker"
+            color = {snap.items[snap.current]} 
+            onChange = {(color) => (state.items[snap.current] = color)} />
+            <h1>{snap.current}</h1>
         </div>
     )
 }
@@ -131,8 +141,11 @@ export default function Main() {
     <Picker/>
     <Canvas camera = {{position: [0,5, 20]}}>
       <ambientLight intensity={0.5} />
+      <spotLight intensity={0.3} position = {[5, 20, 20]}/>
       <Suspense fallback ={null}>
           <Model/>
+          <Environment files = "snowy_forest_path_01_2k.hdr"/>
+          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={2} far={1} />
       </Suspense>
       <OrbitControls />
     </Canvas>
